ccbde866699469f4a9dc2dd6fd995ed586694ae7
Ivan <ivmishevski@gmail.com>
2024-12-08 00:20:13 +0200

    change 3

--- src/app/app.interceptor.ts
+++ src/app/app.interceptor.ts
@@ -1,16 +1,32 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from '../environments/environment';
+import { catchError } from 'rxjs';
+import { inject } from '@angular/core';
+import { ErrorMsgService } from './core/error-msg/error-msg.service';
+import { Router } from '@angular/router';
 
-const {apiUrl} = environment 
+const { apiUrl } = environment
 
 export const appInterceptor: HttpInterceptorFn = (req, next) => {
   const API = '/api';
-  if(req.url.startsWith(API)){
+  if (req.url.startsWith(API)) {
     req = req.clone({
       url: req.url.replace(API, apiUrl),
-      withCredentials:true 
+      withCredentials: true
     });
   }
-  
-  return next(req);
+  const errorMsgService = inject(ErrorMsgService);
+  const router = inject(Router);
+
+  return next(req).pipe(
+    catchError((err) => {
+      if (err.status === 401) {
+        router.navigate(['/login']);
+      } else {
+        errorMsgService.setError(err);
+        router.navigate(['/error'])
+      }
+      return [err];
+    })
+  )
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,40 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ErrorComponent } from './error/error.component';
+import { PageNotFound } from './error/error.component';
 import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
 import { ProfileComponent } from './user/profile/profile.component';
 import { AddThemeComponent } from './theme/add-theme/add-theme.component';
 import { MainComponent } from './main/main.component';
 import { CurrentThemeComponent } from './theme/current-theme/current-theme.component';
+import { ErrorMsgComponent } from './core/error-msg/error-msg.component';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-    {path:'', redirectTo:'/home', pathMatch: 'full'},
-    {path:'home', component:HomeComponent},
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
     //user routing
-    {path:'login',component:LoginComponent},
-    {path:'register',component:RegisterComponent},
-    {path:'profile',component:ProfileComponent},
-    {path:'logout',component:ProfileComponent},
+    { path: 'login', component: LoginComponent,canActivate: [AuthGuard], },
+    { path: 'register', component: RegisterComponent,canActivate: [AuthGuard], },
+    { path: 'profile', component: ProfileComponent,canActivate: [AuthGuard], },
+    { path: 'logout', component: ProfileComponent },
     //start theme routing
-    {path:'themes', children:[
-    {path: '', component:MainComponent},
-    {path: ':themeId',component:CurrentThemeComponent}    
-    ]},
-    {path:'add-theme',component:AddThemeComponent},
-    //end theme routing
-    {path:'404*', component:ErrorComponent},
-    {path:'**', component:ErrorComponent}
+    {
+        path: 'themes', children: [
+            { path: '', component: MainComponent },
+            { path: ':themeId', component: CurrentThemeComponent }
+        ]
+    },
+    {
+        path: 'add-theme',
+        canActivate: [AuthGuard],
+        loadComponent: () =>
+            import('./theme/add-theme/add-theme.component').then((c) => 
+                c.AddThemeComponent
+         )
+    },
+//end theme routing
+{ path: 'error', component: ErrorMsgComponent },
+{ path: '404*', component: PageNotFound },
+{ path: '**', component: PageNotFound }
 ];
